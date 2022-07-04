72ffcd8ae1ad6686a87746def4b65ebce3a51f60
Atreyee <Atreyee@Atreyees-MacBook-Pro.local>
2022-07-05 00:07:15 +0530

    added dict code

--- wav2vec-asr/utils.py
+++ wav2vec-asr/utils.py
@@ -6,33 +6,38 @@ Created on Sun Jan 30 17:55:49 2022
 @author: atreyee
 """
 import time
+import io
+import os
+import string
+import tempfile
+import unittest
 
-def test_add_file_to_dict(self):
-        counts = {}
-        num_lines = 100
-        per_line = 10
-        with tempfile.TemporaryDirectory("test_sampling") as data_dir:
-            filename = os.path.join("dict.ltr.txt")
-            with open(filename, "w", encoding="utf-8") as data:
-                for c in string.ascii_letters:
-                    line = f"{c} " * per_line
-                    print(line)
-                    for _ in range(num_lines):
-                        data.write(f"{line}\n")
-                    counts[c] = per_line * num_lines
-                    per_line += 5
+import torch
+from fairseq import tokenizer
+from fairseq.data import Dictionary
 
-            dict = Dictionary()
-            Dictionary.add_file_to_dictionary(
-                filename, dict, tokenizer.tokenize_line, 10
-            )
-            dict.finalize(threshold=0, nwords=-1, padding_factor=8)
+def add_file_to_dict(data_dir, dict_file_name):
+    counts = {}
+    num_lines = 100
+    per_line = 10
+   
+    filename = os.path.join(data_dir, dict_file_name)
+    with open(filename, "w", encoding="utf-8") as data:
+        for c in string.ascii_letters:
+            line = f"{c} " * per_line
+            print(line)
+            for _ in range(num_lines):
+                data.write(f"{line}\n")
+            counts[c] = per_line * num_lines
+            per_line += 5
 
-            for c in string.ascii_letters:
-                count = dict.get_count(dict.index(c))
-                self.assertEqual(
-                    counts[c], count, f"{c} count is {count} but should be {counts[c]}"
-                )
+    dict = Dictionary()
+    Dictionary.add_file_to_dictionary(
+        filename, dict, tokenizer.tokenize_line, 10
+    )
+    dict.finalize(threshold=0, nwords=-1, padding_factor=8)
+
+    return dict
 
 def prepare_empty_dir(dirs, resume=False):
     """
