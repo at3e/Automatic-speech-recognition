39ae7a3d23192c056c586cea8551b4e9b4049bea
Atreyee SAHA <asaha@vws106.idiap.ch>
2022-07-04 19:26:51 +0200

    add test files

--- wav2vec-asr/utils.py
+++ wav2vec-asr/utils.py
@@ -6,6 +6,34 @@ Created on Sun Jan 30 17:55:49 2022
 @author: atreyee
 """
 import time
+
+def test_add_file_to_dict(self):
+        counts = {}
+        num_lines = 100
+        per_line = 10
+        with tempfile.TemporaryDirectory("test_sampling") as data_dir:
+            filename = os.path.join("dict.ltr.txt")
+            with open(filename, "w", encoding="utf-8") as data:
+                for c in string.ascii_letters:
+                    line = f"{c} " * per_line
+                    print(line)
+                    for _ in range(num_lines):
+                        data.write(f"{line}\n")
+                    counts[c] = per_line * num_lines
+                    per_line += 5
+
+            dict = Dictionary()
+            Dictionary.add_file_to_dictionary(
+                filename, dict, tokenizer.tokenize_line, 10
+            )
+            dict.finalize(threshold=0, nwords=-1, padding_factor=8)
+
+            for c in string.ascii_letters:
+                count = dict.get_count(dict.index(c))
+                self.assertEqual(
+                    counts[c], count, f"{c} count is {count} but should be {counts[c]}"
+                )
+
 def prepare_empty_dir(dirs, resume=False):
     """
     if resume experiment, assert the dirs exist,
